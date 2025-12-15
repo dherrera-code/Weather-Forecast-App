0f6778b8f5a6d906b3a92d86ff4bfe532371ad80
dherrera-code <dherr929@gmail.com>
2025-12-15 11:46:53 -0800

    getData must pass the currentCity variable!

--- Scripts/app.js
+++ Scripts/app.js
@@ -2,9 +2,11 @@
 
 
 let currentCity = "Stockton"
-const getData = async () => {
+const getData = async (currentCity) => {
     const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${currentCity}&appid=41f2b9edc30c8f24fe8fcaca12ae33f7`);
     const data = await response.json();
     console.log(data);
 }
-// getData() 
+// getData(currentCity) 
+// currentCity = "Buckeye"
+// getData(currentCity) 
