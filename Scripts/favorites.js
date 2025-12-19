f87987b87f1e3c53d71a3943d796b702ba7fe7a5
dherrera-code <dherr929@gmail.com>
2025-12-19 13:28:53 -0800

    Refractoring html and css files and fav.js

--- Scripts/favorites.js
+++ Scripts/favorites.js
@@ -1,17 +1,14 @@
-import { getFromLocalStorage, saveFavorites, removeFavoriteCity } from "./localStorage.js"
+import { getFromLocalStorage, removeFavoriteCity } from "./localStorage.js"
 // import {getWeatherIcon, convertKToF} from "./app.js"
 import { API_KEY } from "./environment.js";
-//create functions: create favoriteCards, deleteFavCards
-const inputCity = document.getElementById("inputCity");
 
+const inputCity = document.getElementById("inputCity");
 // Outputs new DOM elements
 const displayFavoriteCity = document.getElementById("displayFavoriteCity");
 
 const getCityData = async (cityName) => {
     const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${API_KEY}`);
     const data = await response.json();
-    // console.log(data);
-    //pass data to a function to display new favorite card
     createFavCity(data)
 }
 const getWeatherIcon = (iconID) => {
@@ -61,7 +58,6 @@ const convertKToF = (Kelvin) => {
 }
 const createFavCity = (cityData) => {
     //create column to be appended to display variable
-    console.log(cityData);
     const mainCol = document.createElement("div")
     mainCol.className = "col my-3";
     const cardContainer = document.createElement('div')
@@ -69,15 +65,16 @@ const createFavCity = (cityData) => {
     cardContainer.style = "font-size: 40px;";
     const topRow = document.createElement("div")
     topRow.className = "row mb-4"
+
     const cityTempCol = document.createElement("div");
     cityTempCol.className = "col ms-4 mt-4 font-imprima"
     const cityName = document.createElement("h2");
-    cityName.textContent = cityData.city.name+", "+cityData.city.country;
+    cityName.textContent = cityData.city.name + ", " + cityData.city.country;
     const currentTemp = document.createElement('p');
     currentTemp.textContent = convertKToF(cityData.list[0].main.temp) + '°';
     cityTempCol.appendChild(cityName);
     cityTempCol.appendChild(currentTemp);
-    //verify that elements were appended appropriately]
+    
     const iconDiv = document.createElement("div");
     iconDiv.className = "col d-flex flex-column"
 
@@ -85,8 +82,8 @@ const createFavCity = (cityData) => {
     deleteBtn.className = "x-button"
     deleteBtn.src = "../WeatherAssets/x-button.png";
     deleteBtn.addEventListener("click", () => {
-        console.log(cityData.city.name+","+cityData.city.country)
-        removeFavoriteCity(cityData.city.name+","+cityData.city.country)
+        console.log(cityData.city.name + "," + cityData.city.country)
+        removeFavoriteCity(cityData.city.name + "," + cityData.city.country)
         mainCol.remove();
     })
 
@@ -97,7 +94,6 @@ const createFavCity = (cityData) => {
 
     iconDiv.appendChild(deleteBtn);
     iconDiv.appendChild(weatherIcon);
-
     topRow.appendChild(cityTempCol)
     topRow.appendChild(iconDiv)
 
@@ -110,18 +106,10 @@ const createFavCity = (cityData) => {
 
     let lowTemp = cityData.list[0].main.temp_min;
     let highTemp = cityData.list[0].main.temp_max;
-    for(let i = 0; i < 4; i++)
-    {
-        if(lowTemp > cityData.list[i].main.temp_min) lowTemp = cityData.list[i].main.temp_min
-        if(highTemp < cityData.list[i].main.temp_max) highTemp = cityData.list[i].main.temp_max
+    for (let i = 0; i < 4; i++) {
+        if (lowTemp > cityData.list[i].main.temp_min) lowTemp = cityData.list[i].main.temp_min
+        if (highTemp < cityData.list[i].main.temp_max) highTemp = cityData.list[i].main.temp_max
     }
-
-    // if (cityData.list[1].main.temp_min > cityData.list[0].main.temp_min)
-    //     lowTemp = cityData.list[0].main.temp_min;
-    // else 
-    //     lowTemp = cityData.list[1].main.temp_min;
-    // if (cityData.list[1].main.temp_max > cityData.list[0].main.temp_max) highTemp = cityData.list[0].main.temp_max;
-    // else highTemp = cityData.list[1].main.temp_max;
 
     hTemp.textContent = `H: ${convertKToF(highTemp)}°`
     lTemp.textContent = `L: ${convertKToF(lowTemp)}°`
@@ -136,8 +124,7 @@ const createFavCity = (cityData) => {
 }
 
 inputCity.addEventListener("keypress", (event) => {
-
-        if (event.key === "Enter") {
+    if (event.key === "Enter") {
         let cityName;
         console.log("you have pressed Enter!");
         // If user entered state code or country code
@@ -149,20 +136,15 @@ inputCity.addEventListener("keypress", (event) => {
                 cityName[index] = i.trim();
                 index++;
             });
-
         }
-        else
-        {
+        else {
             cityName = inputCity.value.trim();
         }
-            //  getCityData(inputCity.value);
         console.log(cityName.toString())
         inputCity.value = "";
         sessionStorage.setItem("searchCity", cityName.toString())
         window.location.href = "../index.html";
-
     }
-    
 });
 const displayFavorites = () => {
     let cityArray = getFromLocalStorage();
@@ -170,9 +152,6 @@ const displayFavorites = () => {
     for (let i = 0; i < cityArray.length; i++) {
         // Call a function to getData from API then create elements to display fav cards!
         getCityData(cityArray[i])
-
     }
-
-
 }
 displayFavorites();
